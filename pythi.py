15ed2c7c0f6b84fec0569f04d702e72e51e9508a
Mick95 <michele.gennaioli@mail.polimi.it>
2018-06-07 18:11:40 +0200

    up

--- pythi.py
+++
@@ -1,7 +0,0 @@
-from cassandra.cluster import Cluster  
-cluster = Cluster()  
-print ("no cassandra server found ") 
-if cluster is None:  
-  print (" no no ")  
-else:  
-  print ("cassandra found") 
